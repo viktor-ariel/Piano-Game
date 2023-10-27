3fdb7c3319734b8b01f86799a9e24a57b9965da6
Viktor Ariel <98894099+viktor-ariel@users.noreply.github.com>
2023-10-26 23:11:54 -0300

    Add files via upload

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -1,13 +1,27 @@
 const pianoKeys = document.querySelectorAll(".piano-keys .key")
 
+let mapedKeys = []
 let audio = new Audio("Tunes/a.wav")
 
 const playTune = key => {
     audio.src = `Tunes/${key}.wav`
     audio.play();
+
+    const clickedkey = document.querySelector(`[data-key="${key}"]`)
+    clickedkey.classList.add("active")
+    setTimeout(()=>{
+        clickedkey.classList.remove("active")
+    },150)
 };
 
 pianoKeys.forEach((key)=>{
     console.log(key.dataset.key);
     key.addEventListener("click",()=> playTune(key.dataset.key))
+    mapedKeys.push(key.dataset.key)
 })
+
+document.addEventListener("keydown", (e)=>{
+    if(mapedKeys.includes(e.key)){
+        playTune(e.key)   
+    } 
+});
